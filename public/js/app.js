a1e7fcd829705b6fa7270a116f20f577ac97b94c
Chris Enríquez <christoferen7@gmail.com>
2016-06-26 11:05:41 -0600

    Cambio nombre carpetas, y carga de imágenes

---
+++ public/js/app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var display, input, frames, speedFrame, 
+	lvFrame, alSprite, taSprite, ciSprite, 
+	aliens, dir, tank, bullets, cities;
+
+function main () {}
+function init () {}
+function run () {}
+function render () {}
+function update () {}
+
+document.addEventListener('DOMContentLoaded', function () {
+	main()
+})
